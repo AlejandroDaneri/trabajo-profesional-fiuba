ab64fbdef7e8bd2b0ef5060279392deed062f8e7
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-11-04 13:13:56 -0300

    script to deploy frontend

---
+++ frontend/deploy.py
@@ -0,0 +1,7 @@
+import os
+os.system('yarn run build')
+os.system('cd build && tar -czvf build.tar.gz *')
+url = 'satoshibot.tech'
+os.system('scp build/build.tar.gz root@{}:/var/www/html/build.tar.gz'.format(url))
+os.system('ssh root@{} "cd /var/www/html/ && tar -xf build.tar.gz"'.format(url))
+os.system('ssh root@{} "service apache2 restart"'.format(url))
