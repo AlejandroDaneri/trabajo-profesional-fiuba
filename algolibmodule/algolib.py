3f5ff739709aed49d8cd559e59e22aae421290a7
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-11-06 11:10:12 -0300

    comments and OBV

--- algolibmodule/algolib.py
+++ algolibmodule/algolib.py
@@ -13,36 +13,44 @@ class AlgoLib:
 
     def RSI(self, rounds):
         data = self.data
+        ## calcula la diferencia del price close entre la fila actual y la anterior
         data['diff'] = data.Close.diff()
-        data['win'] = np.where(data['diff']>0, data['diff'],0)
-        data['loss'] = np.where(data['diff']<0,abs(data['diff']),0)
-        data['EMA_win'] = data.win.ewm(alpha=1/rounds).mean()
-        data['EMA_loss'] = data.loss.ewm(alpha=1/rounds).mean()
+        ## si la diferencia es mayor a 0 setea de cuanto fue, sino es ganancia pone 0
+        data['win'] = np.where(data['diff'] > 0, data['diff'], 0)
+        ## si la diferencia es menor a 0 setea de cuanto fue pero en valor absoluto, sino es ganancia pone 0
+        data['loss'] = np.where(data['diff'] < 0, abs(data['diff']), 0)
+        ## se calcula una media movil exponencial de las ganancias
+        data['EMA_win'] = data.win.ewm(alpha = 1/rounds).mean()
+        ## se calcula una media movil exponencial de las perdidas
+        data['EMA_loss'] = data.loss.ewm(alpha = 1/rounds).mean()
+        ## cociente entre ellas
         data['RS'] = data.EMA_win / data.EMA_loss
-        data['RSI'] = 100 - (100 /(1 + data.RS))
+        ## se calcula finalmente el RSI
+        data['RSI'] = 100 - (100 / (1 + data.RS))
         return data
 
-    """
-    ### Moving Average Converge Divergence ###
-
-                FAST   - SLOW
-    MACD LINE = EMA 12 - EMA 23
-    
-    SIGNAL = EMA 9 (MACD LINE)
-    MACD = SIGNAL - MACD LINE
-
-    ##########################################
-    """
     def MACD(self, slow = 23, fast = 12, suavizado = 9):
         data = self.data
-        data["ema_fast"] = data.Close.ewm(span=fast).mean()
-        data["ema_slow"] = data.Close.ewm(span=slow).mean()
+        ## se calcula una media movil exponencial rapida
+        data["ema_fast"] = data.Close.ewm(span = fast).mean()
+        ## se calcula una media movil exponencial lenta
+        data["ema_slow"] = data.Close.ewm(span = slow).mean()
+        ## la resta de las medias moviles es otra media movil llamada macd
         data["macd"] = data.ema_fast - data.ema_slow
-        data['signal'] = data.macd.ewm(span=suavizado).mean()
+        ## a esta ultima se la suaviza y se la pasa a llamar signal
+        data['signal'] = data.macd.ewm(span = suavizado).mean()
+        ## finalmente el punto de interes es la diferencia entre la media movil macd y la señal
+        ## particularmente es de interes cuando cruza el cero. 
         data['histogram'] = data.macd - data.signal
         data = data.dropna().round(2)
         return data
     
+    def OBV(self, n):
+        data['Balance'] = np.where(data.Close > data.Close.shift(), data['Volume'], np.where(data.Close < data.Close.shift(), -data['Volume'], 0))
+        data['OBV'] = data['Balance'].rolling(n).sum()
+        data['OBV_acotado'] = (data.OBV - data.OBV.rolling(n).min())/(data.OBV.rolling(n).max() - data.OBV.rolling(n).min())
+        return data
+
     def plot_RSI(self):
         data = self.data
         fig = plt.figure()
