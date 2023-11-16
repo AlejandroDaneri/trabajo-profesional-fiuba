8e9d01a724d21f81519859831aca3333180cfa6c
Alejandro Daneri <adaneri@fi.uba.ar>
2023-11-16 07:50:28 -0300

    fix algo_trader

--- algo_trader/main.py
+++ algo_trader/main.py
@@ -3,5 +3,5 @@ print("Trabajo Profesional | Algo Trading | Real Time")
 import time
 
 while(True):
-    print("Algo Trader | Running")
-    time.sleep(60)
+  print("Algo Trader | Running")
+  time.sleep(60)
