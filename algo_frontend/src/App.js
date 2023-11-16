a5b69b30badde1bc311b2fe81cecf326f7f6dec6
Alejandro Daneri <adaneri@fi.uba.ar>
2023-11-16 14:58:21 -0300

    test frontend deploy

--- algo_frontend/src/App.js
+++ algo_frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>SatoshiBOT.tech</p>
+        <p>SatoshiBOT.tech </p>
       </header>
     </div>
   );
