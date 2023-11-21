e0044ee81d9efef7cdc1b5922693baef2a79e77f
fedepenic <fedepenic@hotmail.com>
2023-11-21 10:11:10 -0300

    Add and delete files, update dependencies, and implement user registration and login functionality

--- algo_frontend/src/App.js
+++ algo_frontend/src/App.js
@@ -1,13 +1,42 @@
-import logo from "./bitcoin.png";
-import "./App.css";
+import "./styles/App.css";
+
+import React, { useState } from "react";
+import { createUser, login } from "./config/firebaseConfig";
+
+import logo from "./assets/bitcoin.png";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    await createUser(email, password);
+  };
+
+  const handleLogin = async () => {
+    await login(email, password);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>SatoshiBOT.tech </p>
-      </header>
+      <div className="Login-area">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleRegister}>Register</button>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>SatoshiBOT.tech </p>
     </div>
   );
 }
