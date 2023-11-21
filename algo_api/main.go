04c4c2872c1dc8685e6ef9b9909de3cae1f94584
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-11-20 22:33:27 -0300

    algo api ok

--- algo_api/main.go
+++ algo_api/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+}
+
+func handlerPing(w http.ResponseWriter, r *http.Request) {
+	response, err := json.Marshal("pong")
+	if err != nil {
+		return
+	}
+
+	w.Write(response)
+}
 
 func main() {
-	fmt.Println("¡Hola, mundo!")
+	http.HandleFunc("/ping", handlerPing)
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Servidor escuchando en el puerto 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error al iniciar el servidor:", err)
+	}
 }
