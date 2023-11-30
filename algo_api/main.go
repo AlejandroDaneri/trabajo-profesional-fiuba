7c5586a26dbf100685f7836a1afdd836fdbfc259
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-11-30 13:07:50 -0300

    create ok works

--- algo_api/main.go
+++ algo_api/main.go
@@ -4,8 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/leesper/couchdb-golang"
+	"github.com/sirupsen/logrus"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +24,27 @@ func PingPong(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateTrade(w http.ResponseWriter, r *http.Request) {
+	couchDBUser := os.Getenv("COUCHDB_USER")
+	couchDBPassword := os.Getenv("COUCHDB_PASSWORD")
+	client, err := couchdb.NewServer(fmt.Sprintf("http://%s:%s@couchdb:5984", couchDBUser, couchDBPassword))
+	if err != nil {
+		fmt.Println("Error al conectar a CouchDB:", err)
+		return
+	}
+	dbName := "trades"
+	db, err := client.Get(dbName)
+	if err != nil {
+		logrus.Error("Could not get db")
+		return
+	}
+	doc := map[string]interface{}{
+		"pair": "BTC/USDT",
+	}
+	_, _, err = db.Save(doc, nil)
+	if err != nil {
+		logrus.Error("Could not create doc")
+		return
+	}
 }
 
 func GetTrade(w http.ResponseWriter, r *http.Request) {
