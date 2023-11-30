a21a2d240fc174111f452949fa7c6b67375ab9b8
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-11-30 11:44:22 -0300

    agrego lib mux

--- algo_api/main.go
+++ algo_api/main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 }
 
-func handlerPing(w http.ResponseWriter, r *http.Request) {
+func PingPong(w http.ResponseWriter, r *http.Request) {
 	response, err := json.Marshal("pong")
 	if err != nil {
 		return
@@ -18,12 +20,28 @@ func handlerPing(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
+func CreateTrade(w http.ResponseWriter, r *http.Request) {
+}
+
+func GetTrade(w http.ResponseWriter, r *http.Request) {
+}
+
+func ListTrades(w http.ResponseWriter, r *http.Request) {
+}
+
 func main() {
-	http.HandleFunc("/ping", handlerPing)
-	http.HandleFunc("/", handler)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/ping", PingPong).Methods("POST")
+
+	router.HandleFunc("/trade", CreateTrade).Methods("POST")
+	router.HandleFunc("/trade/{id}", GetTrade).Methods("GET")
+	router.HandleFunc("/trade", ListTrades).Methods("GET")
+
+	router.HandleFunc("/", handler)
 
 	fmt.Println("Servidor escuchando en el puerto 8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		fmt.Println("Error al iniciar el servidor:", err)
 	}
