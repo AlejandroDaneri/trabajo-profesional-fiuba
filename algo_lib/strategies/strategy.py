a5ce3208a3f6853d4f5f0546e02b8fd274dd72d8
Alejandro Daneri <adaneri@fi.uba.ar>
2023-12-03 19:17:11 -0300

    add investment ratio to strategy

--- algo_lib/strategies/strategy.py
+++ algo_lib/strategies/strategy.py
@@ -1,9 +1,14 @@
 from abc import abstractmethod
-
+from typing import List
 from actions import Action
+from indicators.indicator import Indicator
 
 
 class Strategy:
+    def __init__(self, indicators: List[Indicator]):
+        self.indicators = indicators
+        self.investment_ratio = 1
+
     
     ## consumes all historical data and prepare strategy for predictions
     @abstractmethod
