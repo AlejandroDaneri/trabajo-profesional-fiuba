8f27e1bd8d9ce0badd0f357e92c474d9c7621ef2
fedepenic <fedepenic@hotmail.com>
2023-12-08 13:12:50 -0300

    Add login view.

--- algo_frontend/src/App.js
+++ algo_frontend/src/App.js
@@ -1,42 +1,12 @@
 import "./styles/App.css";
 
-import React, { useState } from "react";
-import { createUser, login } from "./config/firebaseConfig";
-
-import logo from "./assets/bitcoin.png";
+import LoginRegisterView from "./views/loginRegisterView";
+import React from "react";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleRegister = async () => {
-    await createUser(email, password);
-  };
-
-  const handleLogin = async () => {
-    await login(email, password);
-  };
-
   return (
-    <div className="App">
-      <div className="Login-area">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleRegister}>Register</button>
-        <button onClick={handleLogin}>Login</button>
-      </div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>SatoshiBOT.tech </p>
+    <div>
+      <LoginRegisterView />
     </div>
   );
 }
---
+++ algo_frontend/src/views/loginRegisterView.js
@@ -0,0 +1,58 @@
+import "../styles/loginRegisterView.css";
+
+import React, { useState } from "react";
+import { createUser, login } from "../config/firebaseConfig";
+
+const LoginRegisterView = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleCreateAccount = async () => {
+    await createUser(email, password);
+  };
+
+  const handleLogin = async () => {
+    await login(email, password);
+  };
+  return (
+    <div className="login-container">
+      <h2>¡Welcome to SatoshiBot!</h2>
+      <form>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button onClick={handleLogin} type="button" className="login-button">
+          Log in
+        </button>
+      </form>
+
+      <div className="login-options">
+        <button
+          type="button"
+          className="register-button"
+          onClick={handleCreateAccount}
+        >
+          Create your account
+        </button>
+        <button type="button" className="forgot-password-button">
+          Forgot your password?
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginRegisterView;
