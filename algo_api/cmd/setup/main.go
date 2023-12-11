255f43140d303a69642965ed1e1631c371796133
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-12-11 19:21:53 -0300

    create db on pipeline

--- algo_api/cmd/setup/main.go
+++ algo_api/cmd/setup/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"algo_api/internal/databaseservice"
+
 	"github.com/sirupsen/logrus"
 )
 
 // go run cmd/setup/main.go
 // example how to run commands on go
 func main() {
-	logrus.Info("Hello World !")
+	logrus.Info("Setup DB")
+	err := databaseservice.GetInstance().CreateDB("trades")
+	if err != nil {
+		logrus.Error("Could not create DB")
+	}
 }
