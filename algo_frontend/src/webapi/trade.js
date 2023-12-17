8a5123d5abece3acdb25e20fe5b6732a0077a000
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-12-17 12:37:40 -0300

    backend url

--- algo_frontend/src/webapi/trade.js
+++ algo_frontend/src/webapi/trade.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const getBackend = () => {
   return process.env.NODE_ENV === "development"
     ? ""
-    : "http://deploy_algo_api_1:8080"
+    : "http://satoshibot.tech:8080"
 }
 
 export const list = () => {
