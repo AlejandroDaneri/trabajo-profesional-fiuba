f8ed6f5a35e70369118de71b05bf0514d74ec403
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2023-12-30 13:14:23 -0300

    #6

--- algo_trader/main_test.py
+++ algo_trader/main_test.py
@@ -1,55 +1,9 @@
-from lib.indicators.crossing import Crossing
-from lib.indicators.rsi import RSI
 from lib.exchanges.dummy import Dummy
-from lib.strategies.basic import Basic
 from lib.trade_bot import TradeBot
 from lib.providers.binance import Binance
+from utils import hydrate_strategy
 
 import requests
-
-def hydrate_indicator_rsi(parameters):
-    if parameters is None:
-        print("indicator rsi not have parameters")
-        return None
-    buy_threshold = parameters["buy_threshold"]
-    sell_threshold = parameters["sell_threshold"]
-    rounds = parameters["rounds"]
-    if buy_threshold is None or sell_threshold is None or rounds is None:
-        print("indicator rsi not have all the parameters")
-        return None
-    return RSI(buy_threshold, sell_threshold, rounds)
-
-def hydrate_indicator_crossing(parameters):
-    if parameters is None:
-        print("indicator crossing not have parameters")
-        return None
-    buy_threshold = parameters["buy_threshold"]
-    sell_threshold = parameters["sell_threshold"]
-    fast = parameters["fast"]
-    slow = parameters["slow"]
-    if buy_threshold is None or sell_threshold is None or fast is None or slow is None:
-        print("indicator crossing not have all the parameters")
-        return None
-    return Crossing(buy_threshold, sell_threshold, fast, slow)
-
-def hydrate_strategy(currencies, indicators):
-    strategies = {}
-    for currency in currencies:
-        indicators_builded = []
-
-        for indicator in indicators:
-            if indicator["name"] == "rsi":
-                rsi = hydrate_indicator_rsi(indicator["parameters"])
-                if rsi is not None:
-                    indicators_builded.append(rsi)
-
-            elif indicator["name"] == "crossing":
-                crossing = hydrate_indicator_crossing(indicator["parameters"])
-                if crossing is not None:
-                    indicators_builded.append(crossing)
-        
-        strategies[currency] = Basic(indicators_builded)
-    return strategies
 
 def main():
     requests.delete(url='http://localhost:8080/api/trade')
@@ -65,8 +19,6 @@ def main():
 
     strategies = hydrate_strategy(currencies, indicators)
     
-    trade_bot = TradeBot(strategies, exchange)
-
     data = {}
     train_data = {}
     simulation_data = {}
@@ -79,6 +31,8 @@ def main():
         train_data[currency] = data[currency].iloc[0:n_train]
         simulation_data[currency] = data[currency].iloc[n_train:(n_train + n_simulate)]
         strategies[currency].train(train_data[currency])
+
+    trade_bot = TradeBot(strategies, exchange)
 
     for index in range(n_simulate):
         
