08a56846abc3b392349224e62fc4500dfeaa08b9
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-01-04 21:10:13 -0300

    #3

--- algo_api/internal/strategyservice/strategyservice.go
+++ algo_api/internal/strategyservice/strategyservice.go
@@ -3,8 +3,8 @@ package strategyservice
 import (
 	"algo_api/internal/database"
 	"algo_api/internal/databaseservice"
+	"algo_api/internal/utils"
 	"encoding/json"
-	"fmt"
 	"sync"
 )
 
@@ -72,33 +72,6 @@ func (s *StrategyService) Get() (*database.StrategyPublicFields, error) {
 	return &strategy.StrategyPublicFields, nil
 }
 
-func StructToMap(value interface{}) (map[string]interface{}, error) {
-	if res, ok := value.(map[string]interface{}); ok {
-		return res, nil
-	}
-
-	valueBytes, err := json.Marshal(value)
-	if err != nil {
-		return nil, fmt.Errorf("error marshalling struct: %w", err)
-	}
-
-	var valueMap map[string]interface{}
-	err = json.Unmarshal(valueBytes, &valueMap)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling struct: %w", err)
-	}
-
-	return valueMap, nil
-}
-
-func ToPrettyPrint(m interface{}) string {
-	b, err := json.MarshalIndent(m, "", "    ")
-	if err == nil {
-		return string(b)
-	}
-	return ""
-}
-
 func (s *StrategyService) SetBalance(balance int) error {
 	strategy, err := s.get()
 	if err != nil {
@@ -107,7 +80,7 @@ func (s *StrategyService) SetBalance(balance int) error {
 
 	strategy.CurrentBalance = balance
 
-	m, err := StructToMap(*strategy)
+	m, err := utils.StructToMap(*strategy)
 	if err != nil {
 		return err
 	}
