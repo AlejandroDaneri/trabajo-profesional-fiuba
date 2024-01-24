328a7dceeb527cdc16ad3ad8dd7645440becb1ba
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-01-24 11:02:23 -0300

    #1

--- algo_frontend/src/components/Table.js
+++ algo_frontend/src/components/Table.js
@@ -1,40 +1,50 @@
 import styled from "styled-components"
 
+const TableWrapper = styled.div`
+  height: 300px;
+  overflow-y: scroll;
+`;
+
 const TableStyle = styled.table`
+  width: 100%;
+
   & th {
     border: 1px solid white;
     text-align: center;
     padding-left: 10px;
     padding-right: 10px;
   }
+
   & td {
     border: 1px solid white;
     text-align: center;
     padding-left: 10px;
     padding-right: 10px;
   }
-`
+`;
 
 const Table = ({ headers, data, buildRow }) => {
   return (
-    <TableStyle>
-      <thead>
-        <tr>
-          {(headers || []).map((header) => (
-            <th>{header.label}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {(data || []).map((row) => (
+    <TableWrapper>
+      <TableStyle>
+        <thead>
           <tr>
-            {buildRow(row).map((field, index) => (
-              <td key={index}>{field}</td>
+            {(headers || []).map((header) => (
+              <th>{header.label}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </TableStyle>
+        </thead>
+        <tbody>
+          {(data || []).map((row) => (
+            <tr>
+              {buildRow(row).map((field, index) => (
+                <td key={index}>{field}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </TableStyle>
+    </TableWrapper>
   )
 }
 
