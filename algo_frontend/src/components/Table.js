cd98ad93d5a23de6ea0a26a94564da92bde05608
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-01-24 11:30:53 -0300

    #3

--- algo_frontend/src/components/Table.js
+++ algo_frontend/src/components/Table.js
@@ -10,9 +10,6 @@ const TableWrapper = styled.div`
     width: 5px;
   }
 
-  &::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  }
 
   &::-webkit-scrollbar-thumb {
     background-color: darkgrey;
