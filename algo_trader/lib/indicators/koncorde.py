082d6492e0fcf292a93a1712b94ede625406e565
Pablo Inoriza <pinoriza@fi.uba.ar>
2024-02-01 16:49:18 -0300

    Adding NVI, PVI, MFI and Koncorde indicators (#53)
    * Adding NVI indicator
    
    * Finish nvi implementation
    
    * Apply fixes to nvi indicator
    
    * Adding PVI indicator
    
    * Adding MFI indicator
    
    * Adding koncorde indicator
    
    * Applying refactor to koncorde indicator
    
    * Applying more refactors to koncorde indicator
    
    * Applying more refactors to koncorde indicator
    
    * Applying parameters to koncorde indicator
    
    * Applying parameters to koncorde indicator
    
    * Adding new indicators in utils

---
+++ algo_trader/lib/indicators/koncorde.py
@@ -0,0 +1,140 @@
+from lib.indicators.indicator import Indicator
+from lib.actions import Action
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from algo_trader.lib.indicators.nvi import NVI
+from algo_trader.lib.indicators.pvi import PVI
+from algo_trader.lib.indicators.mfi import MFI
+from algo_trader.lib.indicators.rsi import RSI
+from algo_trader.lib.indicators.bbands import BBANDS
+
+class KONCORDE(Indicator):
+    def __init__(self, 
+                 rounds, 
+                 rsi_mfi_length = 14, 
+                 bbands_length = 25, 
+                 bbands_factor = 2.0,
+                 storch_length = 21):
+        self.rounds = rounds
+        self.rsi_mfi_length = rsi_mfi_length
+        self.bbands_length = bbands_length
+        self.bbands_factor = bbands_factor
+        self.storch_length = storch_length
+        super().__init__("KONCORDE")
+
+    def calculate(self, data):
+        self.data = data
+        df = pd.DataFrame(index=data.index)
+        self.dates = data.index
+
+        # Copy the 'Close' column from the original data to the DataFrame
+        df["Close"] = data["Close"]
+
+        # Calculate the typical price, its is the average of the maximum, minimum, open and close price
+        typical_price = (data['Open'] + data['High'] + data['Low'] + data['Close']) / 4
+
+        # Calculate Stochastic indicator of typical price
+        storch = self.calc_stoch(typical_price, data, self.storch_length, 3)
+
+        # Calculate the mfi
+        mfi = MFI(0, 0, self.rsi_mfi_length) # buy_threshold and sell_threshold parameters is not used here
+        mfi_values = mfi.calculate(data)
+
+        # Calculate the rsi based in TP value
+        rsi_values = self.calc_rsi(typical_price, self.rsi_mfi_length)
+
+        # Calculate Bollinger Bands oscilator
+        boll_osc = self.calc_bbands_osc(typical_price, self.bbands_length, self.bbands_factor)
+
+        # Calculate values
+        df["BIG_HANDS"] = self.calc_nvi(data, self.rounds)
+        df["TREND"] = (rsi_values + mfi_values + boll_osc + (storch / 3)) / 2
+        df["SMALL_HANDS"] = (df["TREND"] + self.calc_pvi(data, self.rounds))
+        df["TREND_AVG"] = df["TREND"].ewm(span=self.rounds, adjust=False).mean()
+
+        self.output = df
+        return self.output
+
+    # Calculate the NVI to detect large investments (blue graphic)
+    def calc_nvi(self, data, rounds):
+        nvi = NVI(0) # rounds parameter is not used here
+        nvi_values = nvi.calculate(data).NVI
+        return self.calc_volume_index(nvi_values, rounds, 90)
+    
+    # Calculate the PVI to detect small investments (green graphic)
+    def calc_pvi(self, data, rounds):
+        pvi = PVI(0) # rounds parameter is not used here
+        pvi_values = pvi.calculate(data).PVI
+        return self.calc_volume_index(pvi_values, rounds, 90)
+
+    def calc_volume_index(self, vi, avg_rounds, length):
+        vi_avg = vi.ewm(span=avg_rounds, adjust=False).mean()
+        vi_max = vi_avg.rolling(window=length).max()
+        vi_min = vi_avg.rolling(window=length).min()
+        return (vi - vi_avg) * 100 / (vi_max - vi_min)
+    
+    # Calculate Stochastic indicator
+    def calc_stoch(self, src, data, length, smoothFastD):
+        lowestLow = data['Low'].rolling(window=length).min()
+        highestHigh = data['High'].rolling(window=length).max()
+        currentValue = 100 * (src - lowestLow) / (highestHigh - lowestLow)
+        return currentValue.rolling(smoothFastD).mean()
+    
+    # Calculate RSI indicator
+    def calc_rsi(self, src, rounds):
+        rsi = RSI(0, 0, rounds) # buy_threshold and sell_threshold parameters is not used here
+        df = pd.DataFrame(src, columns = ['Close'])
+        return rsi.calculate(df)
+    
+    # Calculate Bollinger Bands oscilator
+    def calc_bbands_osc(self, src, rounds, factor):
+        bbands = BBANDS(rounds, factor)
+        df = pd.DataFrame(src, columns = ['Close'])
+        df_bbands = bbands.calculate(df)
+        ob1 = (df_bbands["UpperBand"] + df_bbands["LowerBand"]) / factor
+        ob2 = df_bbands["UpperBand"] - df_bbands["LowerBand"]
+        return ((src - ob1) / ob2) * 100
+
+    def calc_buy_signals(self):
+        return np.where((self.output.TREND.shift(1) < self.output.TREND_AVG.shift(1)) 
+                        & (self.output.TREND_AVG <= self.output.TREND), True, False)
+    
+    def calc_sell_signals(self):
+        return np.where((self.output.TREND.shift(1) > self.output.TREND_AVG.shift(1)) 
+                        & (self.output.TREND_AVG >= self.output.TREND), True, False)
+    
+    def plot(self):
+        data = pd.DataFrame(self.output, index= self.dates)
+        fig = plt.figure()
+        fig.set_size_inches(30, 5)
+        plt.axhline(0, linestyle='--', linewidth=1.5, color='black')
+        plt.fill_between(data.index, data["SMALL_HANDS"], 0, where=data["SMALL_HANDS"], alpha=0.5, color='green')
+        plt.fill_between(data.index, data["TREND"], 0, where=data["TREND"], alpha=0.5, color='yellow')
+        plt.fill_between(data.index, data["BIG_HANDS"], 0, where=data["BIG_HANDS"], alpha=0.5, color='blue')
+        plt.plot(data.index, data["TREND_AVG"], color='red', linewidth=1)
+        plt.grid()
+        plt.show()
+
+    def predict_signal(self, new_record):
+        new_koncorde_value = self.calculate(pd.concat([self.data, new_record]))
+        sell_signal = self.calc_sell_signals()[-1]
+        buy_signal = self.calc_buy_signals()[-1]
+
+        new_signal = new_koncorde_value.iloc[-1]
+
+        print(f'[KONCORDE] Small hands value: {new_signal.SMALL_HANDS}')
+        print(f'[KONCORDE] Big hands value: {new_signal.BIG_HANDS}')
+        print(f'[KONCORDE] Trend value: {new_signal.TREND}')
+        print(f'[KONCORDE] Trend avg value: {new_signal.TREND_AVG}')
+
+        if sell_signal == True:
+            signal = Action.SELL
+        elif buy_signal == True:
+            signal = Action.BUY
+        else:
+            signal = Action.HOLD
+
+        print(f'[KONCORDE] Signal: {signal}')
+        
+        return signal
---
+++ algo_trader/lib/indicators/nvi.py
@@ -0,0 +1,89 @@
+from lib.actions import Action
+from lib.indicators.indicator import Indicator
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class NVI(Indicator):
+    def __init__(self, rounds):
+        self.rounds = rounds
+        super().__init__("NVI")
+
+    def calculate(self, data):
+        # Disable SettingWithCopyWarning
+        pd.options.mode.chained_assignment = None
+
+        # Create a DataFrame with the same index as the input data
+        self.data = data
+        df = pd.DataFrame(index=data.index)
+        self.dates = data.index
+
+        # Copy the 'Close' column from the original data to the new DataFrame
+        df["Close"] = data["Close"]
+
+        # Calculate the difference in volume between the current and previous rows
+        df["vol_diff"] = data["Volume"].diff()
+
+        # Initialize NVI column in zero
+        df["NVI"] = 0.0
+
+        # Calculate each NVI value based on its previous NVI value
+        for index in range(len(df)):
+            if index > 0:
+                prev_nvi = df.NVI.iloc[index-1]
+                prev_close = df.Close.iloc[index-1]
+                if df.vol_diff.iloc[index] < 0:
+                    nvi = prev_nvi + ( (df.Close.iloc[index] - prev_close) / (prev_close * prev_nvi) )
+                else: 
+                    nvi = prev_nvi
+            else:
+                # Base NVI value is established (1000 is recommended)
+                nvi = 1000
+            df.NVI.iloc[index] = nvi
+        df["NVI_EMA"] = df.NVI.ewm(ignore_na=False, com=self.rounds, adjust=True).mean()
+
+        # Drop innecesary columns
+        df.drop(["vol_diff"], axis=1, inplace=True)
+        
+        self.output = df
+        return self.output
+
+    def calc_buy_signals(self):
+        return np.where((self.output["NVI_EMA"].shift(1) > self.output["NVI"].shift(1)) & 
+                        (self.output["NVI_EMA"] <= self.output["NVI"]), True, False)
+    
+    def calc_sell_signals(self):
+        return np.where((self.output["NVI_EMA"].shift(1) < self.output["NVI"].shift(1)) & 
+                        (self.output["NVI_EMA"] >= self.output["NVI"]), True, False)
+    
+    def plot(self):
+        data = pd.DataFrame(self.output, index=self.dates)
+        fig = plt.figure()
+        fig.set_size_inches(30, 5)
+        plt.plot(data["NVI_EMA"], color='gray', linewidth=1)
+        plt.fill_between(data.index, data["NVI"], data["NVI_EMA"], where=data["NVI"] > data["NVI_EMA"], alpha=0.5, color='green')
+        plt.fill_between(data.index, data["NVI"], data["NVI_EMA"], where=data["NVI"] < data["NVI_EMA"], alpha=0.5, color='red')
+        plt.grid()
+        plt.show()
+
+    def predict_signal(self, new_record):
+        new_nvi = self.calculate(pd.concat([self.data, new_record]))
+        sell_signal = self.calc_sell_signals()[-1]
+        buy_signal = self.calc_buy_signals()[-1]
+
+        new_signal = new_nvi.iloc[-1]
+
+        print(f'[NVI] Current nvi value: {new_signal.NVI}')
+        print(f'[NVI] Current nvi_ema value: {new_signal.NVI_EMA}')
+
+        if sell_signal == True:
+            signal = Action.SELL
+        elif buy_signal == True:
+            signal = Action.BUY
+        else:
+            signal = Action.HOLD
+        
+        print(f'[NVI] Signal: {signal}')
+        
+        return signal
