8d414e2d55ee339de664ac05f886a1ac4b2c6eef
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-02-03 02:56:21 -0300

    1m ok

--- algo_trader/lib/providers/binance.py
+++ algo_trader/lib/providers/binance.py
@@ -4,6 +4,7 @@ import pandas as pd
 from binance.client import Client as BinanceProvider
 from datetime import date, timedelta
 import os
+import math
 
 def cache_get(ticker: str, timeframe: str, day: date):
     # verify if pair folder exists
@@ -39,18 +40,16 @@ class Binance:
     # ticker: example BTCUSDT
     def get_latest_n(self, ticker: str, timeframe: str, n: int):
 
-        # asi como anda para 1H tengo que lograr que ande para
-        # 1M
-
         N_DAYS = {
-            "5M": int(n / (24 * 1 * (5 / 60))) + 1,
-            "15M": int(n / (24 * 1 * (15 / 60))) + 1,
-            "30M": int(n / (24 * 1 * (30 / 60))) + 1,
-            "1H": int(n / (24 * 1 * (60 / 60))) + 1,
-            "4H": int(n / (24 * 4 * (60 / 60))) + 1
+            "1M": math.ceil(n / (24 * 1 * (60 / 1))) + 1,
+            "5M": math.ceil(n / (24 * 1 * (60 / 5))) + 1,
+            "15M": math.ceil(n / (24 * 1 * (60 / 15))) + 1,
+            "30M": math.ceil(n / (24 * 1 * (60 / 30))) + 1,
+            "1H": math.ceil(n / (24 * 1 * (60 / 60))) + 1,
+            "4H": math.ceil(n / (24 * 4 * (60 / 60))) + 1
         }
 
-        if timeframe == "1H" or timeframe == '4H' or timeframe == '5M' or timeframe == '30M' or timeframe == '15M':
+        if timeframe == "1H" or timeframe == '4H' or timeframe == '1M' or timeframe == '5M' or timeframe == '30M' or timeframe == '15M':
             # build days list required to get n rows
             days = []
             for i in reversed(range(N_DAYS[timeframe])):
@@ -91,14 +90,7 @@ class Binance:
         data = data.set_index("Open")
         return data
     
-    # ticker: example BTCUSDT
-    # timeframe: example 1H
-    # day: example 2023-12-08
-    def get_by_day(self, ticker: str, timeframe: str, day: date):
-        data = cache_get(ticker, timeframe, day)
-        if data is not None:
-            return data
-
+    def binance_get(self, ticker: str, timeframe: str, start: int, end: int):
         timeframes = {
             "1M": BinanceProvider.KLINE_INTERVAL_1MINUTE,
             "5M": BinanceProvider.KLINE_INTERVAL_5MINUTE,
@@ -108,6 +100,21 @@ class Binance:
             "4H": BinanceProvider.KLINE_INTERVAL_4HOUR,
             "1D": BinanceProvider.KLINE_INTERVAL_1DAY
         }
+        klines = self.provider.get_historical_klines(ticker, timeframes[timeframe], start_str=start, end_str=end)
+        data = pd.DataFrame(klines, columns = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume"," Number of trades"," Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"])
+        data['Open'] = data['Open time'].apply(lambda x : datetime.fromtimestamp(x / 1000).strftime('%Y-%m-%d %H-%M'))
+        data['Close'] =  data['Close'].apply(lambda x : float(x))
+        data = data.set_index("Open")
+        data = data[:-1]
+        return data
+    
+    # ticker: example BTCUSDT
+    # timeframe: example 1H
+    # day: example 2023-12-08
+    def get_by_day(self, ticker: str, timeframe: str, day: date):
+        data = cache_get(ticker, timeframe, day)
+        if data is not None:
+            return data
 
         start = str(day)
         end = str(day + timedelta(days=1))
@@ -115,12 +122,15 @@ class Binance:
         start__ = int(datetime.timestamp(start_)) * 1000
         end_ = datetime.strptime(end, '%Y-%m-%d')
         end__ = int(datetime.timestamp(end_)) * 1000
-        klines = self.provider.get_historical_klines(ticker, timeframes[timeframe], start_str=start__, end_str=end__)
-        data = pd.DataFrame(klines, columns = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume"," Number of trades"," Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"])
-        data['Open'] = data['Open time'].apply(lambda x : datetime.fromtimestamp(x / 1000).strftime('%Y-%m-%d %H-%M'))
-        data['Close'] =  data['Close'].apply(lambda x : float(x))
-        data = data.set_index("Open")
-        data = data[:-1]
+        
+        data = None
+        if timeframe == "1M":
+            middle = start__ + int((end__ - start__) / 2)
+            data_1 = self.binance_get(ticker, timeframe, start__, end__)
+            data_2 = self.binance_get(ticker, timeframe, middle, end__)
+            data = pd.concat([data_1, data_2])
+        else:
+            data = self.binance_get(ticker, timeframe, start__, end__)
 
         cache_set(ticker, timeframe, day, data)
 
