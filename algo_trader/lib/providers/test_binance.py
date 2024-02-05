340aeb37299b11b4621110e548069c501322fdb6
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-02-05 13:04:12 -0300

    refactored the get_latest_n test

--- algo_trader/lib/providers/test_binance.py
+++ algo_trader/lib/providers/test_binance.py
@@ -1,36 +1,9 @@
 from lib.providers.binance import Binance
-import logging
+
 provider = Binance()
-logging.getLogger().info("boo %s", "arg")
-def test_timeframe_1h():
-    data = provider.get_latest_n('BTCUSDT', '1H', 1000)
-    assert len(data) == 1000
 
-def test_timeframe_1m():
-    data = provider.get_latest_n('BTCUSDT', '1M', 1000)
-    assert len(data) == 1000
-
-def test_timeframe_5m():
-    data = provider.get_latest_n('BTCUSDT', '5M', 1000)
-    assert len(data) == 1000
-
-def test_timeframe_15m():
-    data = provider.get_latest_n('BTCUSDT', '15M', 1000)
-    assert len(data) == 1000
-
-def test_timeframe_1h():
-    data = provider.get_latest_n('BTCUSDT', '1H', 1000)
-    assert len(data) == 1000
-
-def test_timeframe_4h():
-    data = provider.get_latest_n('BTCUSDT', '4H', 1000)
-    assert len(data) == 1000
-
-def test_timeframe_1d():
-    data = provider.get_latest_n('BTCUSDT', '1D', 1000)
-    assert len(data) == 1000
-    
-def test_timeframe_1d_2000():
-    print("hola")
-    data = provider.get_latest_n('BTCUSDT', '1D', 2000)
-    assert len(data) == 2000
+def test_get_latest_n():
+    timeframes = ['1M', '5M', '15M', '1H', '4H', '1D']
+    for timeframe in timeframes:
+        data = provider.get_latest_n('BTCUSDT', timeframe, 1000)
+        assert len(data) == 1000
