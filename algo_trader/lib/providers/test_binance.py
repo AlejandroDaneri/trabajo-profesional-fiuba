a2a8c56193ce1ae394722318f84e9d99cb0e8a22
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-02-05 12:38:50 -0300

    use pytest instead unittest

--- algo_trader/lib/providers/test_binance.py
+++ algo_trader/lib/providers/test_binance.py
@@ -1,37 +1,36 @@
-import unittest
 from lib.providers.binance import Binance
-
+import logging
 provider = Binance()
+logging.getLogger().info("boo %s", "arg")
+def test_timeframe_1h():
+    data = provider.get_latest_n('BTCUSDT', '1H', 1000)
+    assert len(data) == 1000
 
-class TestGetLatestN(unittest.TestCase):
-    def test_timeframe_1h(self):
-        data = provider.get_latest_n('BTCUSDT', '1H', 1000)
-        self.assertEqual(len(data), 1000)
+def test_timeframe_1m():
+    data = provider.get_latest_n('BTCUSDT', '1M', 1000)
+    assert len(data) == 1000
 
-    def test_timeframe_1m(self):
-        data = provider.get_latest_n('BTCUSDT', '1M', 1000)
-        self.assertEqual(len(data), 1000)
+def test_timeframe_5m():
+    data = provider.get_latest_n('BTCUSDT', '5M', 1000)
+    assert len(data) == 1000
 
-    def test_timeframe_5m(self):
-        data = provider.get_latest_n('BTCUSDT', '5M', 1000)
-        self.assertEqual(len(data), 1000)
+def test_timeframe_15m():
+    data = provider.get_latest_n('BTCUSDT', '15M', 1000)
+    assert len(data) == 1000
 
-    def test_timeframe_15m(self):
-        data = provider.get_latest_n('BTCUSDT', '15M', 1000)
-        self.assertEqual(len(data), 1000)
+def test_timeframe_1h():
+    data = provider.get_latest_n('BTCUSDT', '1H', 1000)
+    assert len(data) == 1000
 
-    def test_timeframe_1h(self):
-        data = provider.get_latest_n('BTCUSDT', '1H', 1000)
-        self.assertEqual(len(data), 1000)
+def test_timeframe_4h():
+    data = provider.get_latest_n('BTCUSDT', '4H', 1000)
+    assert len(data) == 1000
 
-    def test_timeframe_4h(self):
-        data = provider.get_latest_n('BTCUSDT', '4H', 1000)
-        self.assertEqual(len(data), 1000)
-
-    def test_timeframe_1d(self):
-        data = provider.get_latest_n('BTCUSDT', '1D', 1000)
-        self.assertEqual(len(data), 1000)
-
-# Running the tests
-if __name__ == '__main__':
-    unittest.main()
+def test_timeframe_1d():
+    data = provider.get_latest_n('BTCUSDT', '1D', 1000)
+    assert len(data) == 1000
+    
+def test_timeframe_1d_2000():
+    print("hola")
+    data = provider.get_latest_n('BTCUSDT', '1D', 2000)
+    assert len(data) == 2000
