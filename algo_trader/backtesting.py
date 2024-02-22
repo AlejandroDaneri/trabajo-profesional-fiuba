89896692e999babf19f5cabe8d79b2371bec9b9e
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-02-22 20:53:55 -0300

    #1

--- algo_trader/backtesting.py
+++ algo_trader/backtesting.py
@@ -1,18 +1,11 @@
 from lib.strategies.basic import Basic
 from lib.indicators.rsi import RSI
 from lib.indicators.crossing import Crossing
-from datetime import datetime
-import pandas as pd
-from cryptocmd import CmcScraper
+from lib.providers.coinmarketcap import CoinMarketCap
 
 # get BTC prices using CMC provider
-scraper = CmcScraper('BTC', start_date='2011-01-01')
-headers, data = scraper.get_data()
-data = pd.DataFrame(data, columns=headers)
-data['Open'] = data['Date'].apply(lambda x : datetime.strptime(x, '%d-%m-%Y').strftime('%Y-%m-%d')) 
-data = data.set_index('Open')
-data = data.sort_index()
-data
+provider = CoinMarketCap()
+data = provider.get('BTC', start='2011-01-01')
 
 # creates indicators and strategy 
 rsi = RSI(40, 60, 14)
---
+++ algo_trader/lib/providers/coinmarketcap.py
@@ -0,0 +1,17 @@
+from cryptocmd import CmcScraper
+import pandas as pd
+from datetime import datetime
+
+class CoinMarketCap:
+    # example:
+    #  ticker: 'BTC'
+    #  start: '2011-01-01'
+    def get(self, ticker: str, start: str):
+        scraper = CmcScraper(ticker, start_date=start)
+        headers, data = scraper.get_data()
+
+        data = pd.DataFrame(data, columns=headers)
+        data['Open'] = data['Date'].apply(lambda x : datetime.strptime(x, '%d-%m-%Y').strftime('%Y-%m-%d'))
+        data = data.set_index('Open')
+        data = data.sort_index()
+        return data
