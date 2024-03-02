ab02b12e6eddd26342b8aea3aa7e70fd83c2c717
Pablo Inoriza <pablo-ino@hotmail.com>
2024-03-02 20:58:24 -0300

    Adding indicators for EMA and SMA

--- algo_trader/lib/indicators/sma.py
+++ algo_trader/lib/indicators/sma.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 
 
 class SMA(Indicator):
-    def __init__(self, fast_rounds, slow_rounds):
-        self.fast_rounds = fast_rounds
-        self.slow_rounds = slow_rounds
+    def __init__(self, rounds):
+        self.rounds = rounds
         super().__init__("SMA")
 
     def calculate(self, data, normalize=False):
@@ -20,33 +19,30 @@ class SMA(Indicator):
         # Copy the 'Close' column from the original data to the new DataFrame
         df["Close"] = data["Close"]
 
-        # Calculate the fast average of the last n rounds of close
-        df["FAST_SMA"] = data["Close"].rolling(self.fast_rounds).mean()
-
-        # Calculate the slow average of the last n rounds of close
-        df["SLOW_SMA"] = data["Close"].rolling(self.slow_rounds).mean()
+        # Calculate the average of the last n rounds of close
+        df["SMA"] = data["Close"].rolling(self.rounds).mean()
 
         self.df_output = df
         return self.df_output
 
     def calc_buy_signals(self):
         return self._calc_buy_signals(
-            (self.df_output.FAST_SMA.shift(1) < self.df_output.SLOW_SMA.shift(1))
-            & (self.df_output.SLOW_SMA <= self.df_output.FAST_SMA)
+            (self.df_output.Close.shift(1) < self.df_output.SMA.shift(1))
+            & (self.df_output.SMA <= self.df_output.Close)
         )
 
     def calc_sell_signals(self):
         return self._calc_sell_signals(
-            (self.df_output.SLOW_SMA.shift(1) < self.df_output.FAST_SMA.shift(1))
-            & (self.df_output.FAST_SMA <= self.df_output.SLOW_SMA)
+            (self.df_output.SMA.shift(1) < self.df_output.Close.shift(1))
+            & (self.df_output.Close <= self.df_output.SMA)
         )
 
     def plot(self):
         data = pd.DataFrame(self.df_output, index=self.dates)
         fig = plt.figure()
         fig.set_size_inches(30, 5)
-        plt.plot(data.index, data.FAST_SMA, color="green", linewidth=1)
-        plt.plot(data.index, data.SLOW_SMA, color="red", linewidth=1)
+        plt.plot(data.index, data.SMA, color="green", linewidth=1)
+        plt.plot(data.index, data.Close, color="blue", linewidth=1)
         plt.grid()
         plt.show()
 
@@ -55,8 +51,8 @@ class SMA(Indicator):
 
         new_signal = new_df.iloc[-1]
 
-        print(f"[SMA] Current fast SMA value: {new_signal.FAST_SMA}")
-        print(f"[SMA] Current slow SMA value: {new_signal.SLOW_SMA}")
+        print(f"[SMA] Current SMA value: {new_signal.SMA}")
+        print(f"[SMA] Current Close value: {new_signal.Close}")
 
         signal = self.get_last_signal(True)
 
