f7934058e4a9f5ffcc46dfa54063b301482a88dc
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-03-02 11:43:09 -0300

    uso de dates en el eje x

--- algo_trader/lib/utils/plotter.py
+++ algo_trader/lib/utils/plotter.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 def plot(data: pd.DataFrame):
+    # matplotlib works better if we set dates instead strings
+    # on the axis x, the plot looks much better 
+    start = datetime.strptime(data.iloc[0].Date, '%d-%m-%Y')
+    end = datetime.strptime(data.iloc[-1].Date, '%d-%m-%Y')
+    dates = []
+    for n in range(int((end - start).days + 1)):
+        dates.append(start + timedelta(days=n))
+
     fig = plt.figure()
     fig.set_size_inches(30, 5)
-    plt.plot(data.index, data.Close)
+    plt.plot(dates, data.Close)
     plt.show()
