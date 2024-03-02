5b827dd4e6d4eeac1c7f166ff166bf48aa8075df
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-03-02 14:51:49 -0300

    feat(util): plotter (#68)

---
+++ algo_trader/lib/utils/plotter.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+def plot(x: pd.Series, y: pd.Series, log_scale=False):
+    # matplotlib works better if we set dates instead strings
+    # on the axis x, the plot looks much better 
+    start = datetime.strptime(x[0], '%d-%m-%Y')
+    end = datetime.strptime(x[-1], '%d-%m-%Y')
+    dates = []
+    for n in range(int((end - start).days + 1)):
+        dates.append(start + timedelta(days=n))
+
+    fig = plt.figure()
+    fig.set_size_inches(30, 5)
+    if log_scale:
+      plt.yscale('log')
+    plt.plot(dates, y)
+    plt.show()
