2d8b04c1f143b3b0c6c0d2effeee9a05dd96cc7a
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-03-02 11:57:04 -0300

    soporte escala logaritmica

--- algo_trader/lib/utils/plotter.py
+++ algo_trader/lib/utils/plotter.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
-def plot(data: pd.DataFrame):
+def plot(data: pd.DataFrame, log_scale=False):
     # matplotlib works better if we set dates instead strings
     # on the axis x, the plot looks much better 
     start = datetime.strptime(data.iloc[0].Date, '%d-%m-%Y')
@@ -13,5 +13,7 @@ def plot(data: pd.DataFrame):
 
     fig = plt.figure()
     fig.set_size_inches(30, 5)
+    if log_scale:
+      plt.yscale('log')
     plt.plot(dates, data.Close)
     plt.show()
