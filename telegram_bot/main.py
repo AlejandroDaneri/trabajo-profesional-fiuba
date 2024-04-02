6310de14ae96a815c7960f2f982a72e978d4a234
fedepenic <fedepenic@hotmail.com>
2024-04-01 21:44:00 -0300

    Add error handler when two instances of the telegram bot are running.

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,11 +1,15 @@
 from telegram.ext import Updater, CommandHandler
 import os
 
+def error_handler(update, context):
+    print("Two or more instances of the telegram-bot are running. This may result in delays in the updates.")
+
 def start(update, context):
     update.message.reply_text('Welcome to SatoshiBot! You were successfully subscribed to the Telegram messages.')
 
 def main():
     updater = Updater(os.environ.get('BOT_TOKEN'), use_context=True)
+    updater.dispatcher.add_error_handler(error_handler)
 
     dp = updater.dispatcher
 
