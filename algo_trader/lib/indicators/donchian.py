c837a4e4c41fe4ac0087a34ca856e1cb3c776c4b
Alejandro Daneri <adaneri@fi.uba.ar>
2024-04-15 12:07:50 -0300

    add donchain

---
+++ algo_trader/lib/indicators/donchian.py
@@ -0,0 +1,42 @@
+from lib.actions import Action
+from lib.indicators.indicator import Indicator
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class DONCHIAN(Indicator):
+    def __init__(self, periods):
+        self.periods = periods
+        super().__init__("DONCHIAN")
+
+    def calculate(self, data):
+        self.data = data
+        df = pd.DataFrame(index=data.index)
+        df["HighChannel"] = data["High"].rolling(self.periods).max()
+        df["LowChannel"] = data["Low"].rolling(self.periods).min()
+        df["MidChannel"] = (df["HighChannel"] + df["LowChannel"]) / 2
+        self.df_output = df.dropna()
+        return self.df_output
+
+    def calc_buy_signals(self):
+        return self.data["Close"] > self.df_output["HighChannel"]
+
+    def calc_sell_signals(self):
+        return self.data["Close"] < self.df_output["LowChannel"]
+
+    def plot(self):
+        plt.figure(figsize=(12, 6))
+        plt.plot(self.data.index, self.data["Close"], label="Close Price")
+        plt.plot(self.df_output.index, self.df_output["HighChannel"], label="Upper Donchian Channel", linestyle="--")
+        plt.plot(self.df_output.index, self.df_output["LowChannel"], label="Lower Donchian Channel", linestyle="--")
+        plt.title("Donchian Channels Indicator")
+        plt.legend()
+        plt.show()
+
+    def predict_signal(self, new_record, as_enum=True):
+        new_df = self.calculate(pd.concat([self.data, new_record]))
+
+        new_signal = new_df.iloc[-1]
+
+        signal = self.get_last_signal(as_enum)
+        return signal
