999fe65d2c5c184807fe38a3697ef044cd3b9793
Alejandro Daneri <adaneri@fi.uba.ar>
2024-04-15 12:07:55 -0300

    add ewo

---
+++ algo_trader/lib/indicators/ewo.py
@@ -0,0 +1,42 @@
+from lib.actions import Action
+from lib.indicators.indicator import Indicator
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class EWO(Indicator):
+    def __init__(self, short_period, long_period):
+        self.short_period = short_period
+        self.long_period = long_period
+        super().__init__("EWO")
+
+    def calculate(self, data):
+        self.data = data
+        df = pd.DataFrame(index=data.index)
+        ema_short = data["Close"].ewm(span=self.short_period, adjust=False).mean()
+        ema_long = data["Close"].ewm(span=self.long_period, adjust=False).mean()
+        df["EWO"] = ema_short - ema_long
+        self.df_output = df.dropna()
+        return self.df_output
+
+    def calc_buy_signals(self):
+        return self.df_output["EWO"] > 0
+
+    def calc_sell_signals(self):
+        return self.df_output["EWO"] < 0
+
+    def plot(self):
+        plt.figure(figsize=(12, 6))
+        plt.plot(self.df_output.index, self.df_output["EWO"], label=f"EWO ({self.short_period},{self.long_period})")
+        plt.axhline(0, color='black', linestyle='--')
+        plt.title("Elliott Wave Oscillator (EWO) Indicator")
+        plt.legend()
+        plt.show()
+
+    def predict_signal(self, new_record, as_enum=True):
+        new_df = self.calculate(pd.concat([self.data, new_record]))
+
+        new_signal = new_df.iloc[-1]
+
+        signal = self.get_last_signal(as_enum)
+        return signal
