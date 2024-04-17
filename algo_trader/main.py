8b7cb99a389aea73d5f28dbc2e70debb14199236
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-04-17 19:07:10 -0300

    feat(exchange-ui): show used (#112)

--- algo_trader/main.py
+++ algo_trader/main.py
@@ -20,8 +20,8 @@ def main():
     while True:
         strategy_id = list(strategy.values())[0].get_id()
         print(f"[main] running strategy id {strategy_id}")
-        is_running = api.get(f'api/strategy/{list(strategy.values())[0].get_id()}/is_running')
-        if (is_running):     
+        is_running = api.get(f'api/strategy/{strategy_id}/is_running')
+        if (is_running):
             inject_new_tick_to_trade_bot(strategy, trade_bot, data_provider, api)
         else:
             strategy = get_current_strategy(data_provider, api)
