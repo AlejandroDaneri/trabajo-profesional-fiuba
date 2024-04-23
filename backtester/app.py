52199fa1dd8159e3230472ae0e79fa59f6511e4a
Alejandro Daneri <adaneri@fi.uba.ar>
2024-04-23 17:06:13 -0300

    moved gunicorn routes to routes.py

--- backtester/app.py
+++ backtester/app.py
@@ -1,13 +1,5 @@
-from flask import Flask
 
-# Crear una instancia de la aplicación Flask
-app = Flask(__name__)
+from routes import app
 
-# Ruta de ejemplo
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-# Punto de entrada para la aplicación WSGI
 if __name__ == '__main__':
     app.run()
