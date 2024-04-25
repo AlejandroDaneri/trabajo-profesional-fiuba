2de62f0e5078ad54c4bde0673c37158c526b7903
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-04-24 21:48:36 -0300

    connection golang api with python api

--- algo_api/internal/pythonservice/pythonservice.go
+++ algo_api/internal/pythonservice/pythonservice.go
@@ -1,7 +1,13 @@
 package pythonservice
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
 	"sync"
+
+	"github.com/sirupsen/logrus"
 )
 
 var instance IService
@@ -22,9 +28,41 @@ func NewService() IService {
 }
 
 type IService interface {
-	GetBacktesting() error
+	GetBacktesting(coin string, initial_balance string, start string, end string) (Backtesting, error)
 }
 
-func (s *PythonService) GetBacktesting() error {
-	return nil
+type Backtesting struct {
+	FinalBalance string `json:"final_balance"`
+}
+
+func (s *PythonService) GetBacktesting(coin string, initial_balance string, start string, end string) (Backtesting, error) {
+	baseURL := "http://backtester:5000"
+	endpoint := "backtest"
+
+	params := url.Values{}
+	params.Add("coin", coin)
+	params.Add("initial_balance", initial_balance)
+	params.Add("data_from", start)
+	params.Add("data_to", end)
+
+	url := baseURL + "/" + endpoint + "?" + params.Encode()
+
+	response, err := http.Get(url)
+	if err != nil {
+		return Backtesting{}, err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return Backtesting{}, err
+	}
+
+	var backtesting Backtesting
+	err = json.Unmarshal(body, &backtesting)
+	if err != nil {
+		logrus.Error("Could not unmarshal")
+		return Backtesting{}, err
+	}
+	return backtesting, nil
 }
