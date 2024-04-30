f9830525d5f87e93dadcb2a10a062f642f119992
Pablo Inoriza <pablo-ino@hotmail.com>
2024-04-30 02:07:26 -0300

    AT78 - Fixing issues

--- algo_trader/lib/strategies/TDstrategy.py
+++ algo_trader/lib/strategies/TDstrategy.py
@@ -1,7 +1,9 @@
+from lib.actions import Action
 from lib.indicators.indicator import Indicator
 from lib.strategies.strategy import Strategy
-from algo_trader.lib.strategies.base_strategies.TrendDetectionBase import TrendDetectionBase
+from lib.strategies.base_strategies.TrendDetectionBase import TrendDetectionBase
 from typing import List
+from collections import Counter
 
 import pandas as pd
 import numpy as np
@@ -13,9 +15,10 @@ class TDstrategy(Strategy):
         # TODO: check to parameterize this data
         self.rounds_ema = 14
         self.rounds_dmi = 5
+        self.rounds_adx = 5
         self.threshold_adx = 20
         self.period_slope = 5
-        self.tdb = TrendDetectionBase(self.rounds_ema, self.rounds_dmi, self.threshold_adx, self.period_slope)   
+        self.tdb = TrendDetectionBase(self.rounds_ema, self.rounds_dmi, self.rounds_adx, self.threshold_adx, self.period_slope)   
 
         super().__init__(indicators, timeframe, id)
 
@@ -26,33 +29,39 @@ class TDstrategy(Strategy):
         self.data["Low"] = self.data["Low"].apply(lambda x: float(x))
         self.data["Close"] = self.data["Close"].apply(lambda x: float(x))
         self.data["Volume"] = self.data["Volume"].apply(lambda x: float(x))
-        self.data["Open"] = self.data["Open"].apply(lambda x: float(x))
-
-        confirmed_signals = self.tdb.get_confirmed_signals(self, self.data)
-        
-        for indicator in self.indicators:
-            self.data[indicator.name] = None
-            indicator.calculate(self.data, False)
-            self.data[indicator.name] = np.where(indicator.generate_signals() == confirmed_signals, confirmed_signals, 0)
-        
+        self.data["Open"] = self.data["Open"].apply(lambda x: float(x))        
         print(self.name + " | end prepare")
 
     def predict(self, new_record):
+        new_record = new_record[["Open", "High", "Low", "Close","Volume"]].copy()
+        new_record["High"] = new_record["High"].apply(lambda x: float(x))
+        new_record["Low"] = new_record["Low"].apply(lambda x: float(x))
+        new_record["Close"] = new_record["Close"].apply(lambda x: float(x))
+        new_record["Volume"] = new_record["Volume"].apply(lambda x: float(x))
+        new_record["Open"] = new_record["Open"].apply(lambda x: float(x))
 
-        # List to store predicted signals from each indicator
+        print(self.name + " | prediction")
+
+        self.data = pd.concat([self.data, new_record])
+
+        confirmed_signals = self.tdb.get_confirmed_signals(self.data)
+
         signals = []
-
-        # Get predicted signals from each indicator
+        
         for indicator in self.indicators:
-            signal = indicator.predict_signal(new_record)
-            signals.append(signal)
+            indicator.calculate(self.data, False)
+            signal = np.where(indicator.generate_signals() == confirmed_signals, confirmed_signals, 0)
+            signals.append(signal[signal.size -1])
 
-        # Count the frequency of each signal
         signal_counter = Counter(signals)
+        result_signal = signal_counter.most_common(1)[0][0]
 
-        # Get the most common signal
-        most_common_signal = signal_counter.most_common(1)[0][0]
-
-        print(f'[Strategy | Basic] Signal: {most_common_signal}')
-
-        return most_common_signal
+        if result_signal == -1:
+            result_signal = Action.SELL
+        elif result_signal == 1:
+            result_signal = Action.BUY
+        else:
+            result_signal = Action.HOLD
+        print("signal", result_signal)
+        return result_signal
+    
--- algo_trader/lib/strategies/base_strategies/TrendDetectionBase.py
+++ algo_trader/lib/strategies/base_strategies/TrendDetectionBase.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 
 class TrendDetectionBase():
-    def __init__(self, rounds_ema, rounds_dmi, threshold_adx, period_slope):
+    def __init__(self, rounds_ema, rounds_dmi, rounds_adx, threshold_adx, period_slope):
         self.rounds_ema = rounds_ema
         self.rounds_dmi = rounds_dmi
+        self.rounds_adx = rounds_adx
         self.threshold_adx = threshold_adx
         self.period_slope = period_slope
-        super().__init__(rounds_ema, rounds_dmi, threshold_adx, period_slope)
 
     def get_confirmed_signals(self, data):
         df = pd.DataFrame(index=data.index)
@@ -24,10 +24,10 @@ class TrendDetectionBase():
         ema = EMA(self.rounds_ema)
         ema_df = ema.calculate(df)
         df['EMA'] = ema_df.EMA
-        df['Slope'] = self.calculate_slope(df['EMA'], self.period_slope)
+        df['Slope'] = self.calculate_slope(df['EMA'])
 
         # Trend Confirmation
-        dmi = DMI(self.rounds_dmi, self.rounds_dmi, self.threshold_adx)
+        dmi = DMI(self.rounds_dmi, self.rounds_adx, self.threshold_adx)
         dmi_df = dmi.calculate(df)
 
         df['ADX'] = dmi_df['ADX']
@@ -35,8 +35,8 @@ class TrendDetectionBase():
         df['-di'] = dmi_df['-di']
 
         # Define the trend signals based on ADX
-        df['TrendSignal+'] = np.where((self.threshold < df.ADX) & (df['-di'] < df['+di']), 1, 0)
-        df['TrendSignal-'] = np.where((self.threshold < df.ADX) & (df['-di'] > df['+di']), -1, 0)
+        df['TrendSignal+'] = np.where((self.threshold_adx < df.ADX) & (df['-di'] < df['+di']), 1, 0)
+        df['TrendSignal-'] = np.where((self.threshold_adx < df.ADX) & (df['-di'] > df['+di']), -1, 0)
         df['TrendSignal'] = df['TrendSignal+'] + df['TrendSignal-']
 
         confirmedEntrySignals = np.where((0 < df.Slope) & (df['TrendSignal'] == 1), 1, 0)
