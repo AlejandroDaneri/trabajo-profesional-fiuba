c6ff5da5b7ae582d7a37c54d398bee68a76b0fc6
fedepenic <fedepenic@hotmail.com>
2024-05-03 19:50:10 -0300

    Add frontend to select provider.

--- algo_frontend/src/views/Exchanges.js
+++ algo_frontend/src/views/Exchanges.js
@@ -1,25 +1,61 @@
-/* Import Libs */
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
 
-/* Import Reusables Components */
-import View from '../components/reusables/View'
+import Button from "../components/Button";
+import FieldSelect from "../components/reusables/FieldSelect";
+import Input from "../components/reusables/Input";
+import View from "../components/reusables/View";
+import styled from "styled-components";
 
 const ExchangesStyle = styled.div`
-`
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+`;
 
 const Exchanges = () => {
-    return (
-        <>
-            <View
-                title="Exchanges"
-                content={
-                    <ExchangesStyle>
-                    </ExchangesStyle>
-                }
-            />
-        </>
-    )
-}
+  const [selectedOption, setSelectedOption] = useState("");
+  const options = [{ value: "binance", label: "Binance" }];
 
-export default Exchanges
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.value);
+  };
+  return (
+    <>
+      <View
+        title="Exchanges"
+        content={
+          <ExchangesStyle>
+            <form>
+              <h2 style={{ textAlign: "center" }}>Select your provider:</h2>
+              <FieldSelect
+                value={selectedOption}
+                name="selectProvider"
+                onChange={handleSelectChange}
+                options={options}
+                multiple={false}
+                width="30rem"
+              />
+              <h2 style={{ textAlign: "center" }}>Write your API key:</h2>
+              <Input width="30rem" />
+              <h2 style={{ textAlign: "center" }}>Write your API secret:</h2>
+              <Input width="30rem" type={"password"} />
+              <div
+                style={{
+                  marginTop: "2rem",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Button text={"SAVE"} height={40} width={100} />
+              </div>
+            </form>
+          </ExchangesStyle>
+        }
+      />
+    </>
+  );
+};
+
+export default Exchanges;
