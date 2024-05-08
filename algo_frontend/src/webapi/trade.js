80cb09fa4a17b8da95c4d304b165be4ae4fb7329
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-05-08 19:17:23 -0300

    refactor(webapi): getBackend move to env file

--- algo_frontend/src/webapi/trade.js
+++ algo_frontend/src/webapi/trade.js
@@ -1,10 +1,5 @@
 import axios from "axios"
-
-export const getBackend = () => {
-  return process.env.NODE_ENV === "development"
-    ? ""
-    : "https://api.satoshibot.tech"
-}
+import { getBackend } from "./env"
 
 export const list = (strategyID) => {
   return axios.get(`${getBackend()}/api/trades/strategy/${strategyID}`)
