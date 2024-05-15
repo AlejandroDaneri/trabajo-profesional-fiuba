cb53fbee653e83e31c28740c0244f397195e56c1
fedepenic <fedepenic@hotmail.com>
2024-05-15 00:25:32 -0300

    Save chat ids in db every time the user presses start

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,15 +1,26 @@
 from telegram.ext import Updater, CommandHandler
 import os
+import requests
 
-def error_handler(update, context):
-    print("Two or more instances of the telegram-bot are running. This may result in delays in the updates.")
+from api_client import ApiClient
+
+api = ApiClient()
+
+chat_ids = []
 
 def start(update, context):
     update.message.reply_text('Welcome to SatoshiBot! You were successfully subscribed to the Telegram messages.')
+    chat_id = update.message.chat_id
+    if chat_id not in chat_ids:
+        chat_ids.append(chat_id)
+        response_telegram_api = api.post('telegram/chat', json={"chat_id": chat_id})
+        if response_telegram_api.status_code // 100 == 2:
+            print("Chat ID posted successfully:", chat_id)
+        else:
+            print("Failed to post chat ID. Status code:", response_telegram_api.status_code)
 
 def main():
     updater = Updater(os.environ.get('BOT_TOKEN'), use_context=True)
-    updater.dispatcher.add_error_handler(error_handler)
 
     dp = updater.dispatcher
 
