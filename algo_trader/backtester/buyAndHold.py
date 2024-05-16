8c207c5b54eee52c61d9738d0f284828c75ab786
Alejandro Daneri <adaneri@fi.uba.ar>
2024-05-15 23:24:47 -0300

    delete unused code

--- algo_trader/backtester/buyAndHold.py
+++ algo_trader/backtester/buyAndHold.py
@@ -3,16 +3,9 @@ import numpy as np
 from typing import Tuple
 
 class BuyAndHoldBacktester:
-    def __init__(self, initial_balance: float, historical_data: pd.DataFrame, fixed_commission: float=0.005, variable_commission_rate: float=0.005):
+    def __init__(self, initial_balance: float, historical_data: pd.DataFrame):
         self.initial_balance = initial_balance
         self.historical_data = historical_data
-        self.fixed_commission = fixed_commission
-        self.variable_commission_rate = variable_commission_rate
-        self.strategy_linear_returns = None
-        self.strategy_returns = None
-        self.benchmark_returns = None
-        self.strategy_log_returns = None
-        self.benchmark_log_returns = None
 
     def backtest(self) -> Tuple[pd.DataFrame, float]:
         trades, final_balance = self._execute_backtest()
@@ -36,11 +29,8 @@ class BuyAndHoldBacktester:
             "buy_price": [buy_price],
             "sell_date": [self.historical_data.index[-1]],
             "sell_price": [sell_price],
-            "fixed_commission": [self.fixed_commission],
-            "variable_commission": [buy_price * self.variable_commission_rate],
             "return": [(sell_price / buy_price) - 1],
             "cumulative_return": [final_balance / self.initial_balance - 1],
-            "result": ["Winner" if (sell_price / buy_price) > 1 else "Loser"]
         })
 
         return trades, final_balance
