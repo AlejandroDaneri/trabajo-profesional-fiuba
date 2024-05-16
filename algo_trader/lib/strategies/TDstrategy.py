4c012a1517f623f0b4c575973061e4ef1e51c08d
Alejandro Daneri <adaneri@fi.uba.ar>
2024-05-16 00:21:38 -0300

    adding TD strategy backtest

--- algo_trader/lib/strategies/TDstrategy.py
+++ algo_trader/lib/strategies/TDstrategy.py
@@ -2,7 +2,7 @@ from lib.actions import Action
 from lib.indicators.indicator import Indicator
 from lib.strategies.strategy import Strategy
 from lib.strategies.base_strategies.TrendDetectionBase import TrendDetectionBase
-from typing import List
+from typing import List, Tuple
 from collections import Counter
 
 import pandas as pd
@@ -24,25 +24,18 @@ class TDstrategy(Strategy):
 
     def prepare_data(self, historical_data: pd.DataFrame):
         print(self.name + " | begin prepare")
-        self.data = historical_data[["Open", "High", "Low", "Close","Volume"]].copy()
-        self.data["High"] = self.data["High"].apply(lambda x: float(x))
-        self.data["Low"] = self.data["Low"].apply(lambda x: float(x))
-        self.data["Close"] = self.data["Close"].apply(lambda x: float(x))
-        self.data["Volume"] = self.data["Volume"].apply(lambda x: float(x))
-        self.data["Open"] = self.data["Open"].apply(lambda x: float(x))        
+        self.data = historical_data[["Open", "High", "Low", "Close", "Volume"]]
+        self.data[["High", "Low", "Close", "Volume", "Open"]] = self.data[["High", "Low", "Close", "Volume", "Open"]].apply(pd.to_numeric)
         print(self.name + " | end prepare")
 
     def predict(self, new_record):
-        new_record = new_record[["Open", "High", "Low", "Close","Volume"]].copy()
-        new_record["High"] = new_record["High"].apply(lambda x: float(x))
-        new_record["Low"] = new_record["Low"].apply(lambda x: float(x))
-        new_record["Close"] = new_record["Close"].apply(lambda x: float(x))
-        new_record["Volume"] = new_record["Volume"].apply(lambda x: float(x))
-        new_record["Open"] = new_record["Open"].apply(lambda x: float(x))
+        new_record[["High", "Low", "Close", "Volume", "Open"]] = new_record[["High", "Low", "Close", "Volume", "Open"]].apply(pd.to_numeric)
 
-        print(self.name + " | prediction")
+        # print(self.name + " | prediction")
 
-        self.data = pd.concat([self.data, new_record])
+        self.data = pd.concat([self.data, new_record], ignore_index=True)
+        oldest_index = self.data.index[0]
+        self.data = self.data.drop(oldest_index)
 
         confirmed_signals_df = self.tdb.get_confirmed_signals(self.data)
 
@@ -65,6 +58,67 @@ class TDstrategy(Strategy):
             result_signal = Action.BUY
         else:
             result_signal = Action.HOLD
-        print("signal", result_signal)
+        # print("signal", result_signal)
         return result_signal
     
+    def execute_backtest(self, historical_data: pd.DataFrame, initial_balance, fixed_commission, variable_commission_rate) -> Tuple[pd.DataFrame, float]:
+        split_index = 20 # FIXME
+        
+        first_data = historical_data.iloc[:split_index]
+        last_data = historical_data.iloc[split_index:]
+
+        self.prepare_data(first_data)
+        signals = []
+        for index, row in last_data.iterrows():
+            row_df = pd.DataFrame(row).transpose()
+            signal = self.predict(row_df)
+            signals.append(signal)
+
+        actions = pd.DataFrame({'signal': signals}, index=last_data.index)
+
+
+
+        trades = self._get_trades(actions, fixed_commission, variable_commission_rate)
+        final_balance = self._calculate_final_balance(last_data, trades, initial_balance)
+
+        return trades, final_balance
+    
+
+    def _get_trades(self, actions: pd.DataFrame, fixed_commission, variable_commission_rate) -> pd.DataFrame:
+        # WIP
+        pairs = actions.loc[actions['signal'] == Action.BUY]
+        odds = actions.loc[actions['signal'] == Action.SELL]
+
+        trades = pd.concat([pairs, odds], axis=1)
+        trades.columns = ['buy_date', 'buy_signal', 'sell_date', 'sell_signal']
+        trades['buy_price'] = trades['buy_signal'].apply(lambda x: x if x != Action.BUY else np.nan)
+        trades['sell_price'] = trades['sell_signal'].apply(lambda x: x if x != Action.SELL else np.nan)
+
+        trades = trades.dropna()
+
+        trades['commission'] = fixed_commission + trades['buy_price'] * variable_commission_rate
+        trades['buy_price'] += trades['commission']
+        trades['sell_price'] -= trades['commission']
+
+        trades['return'] = (trades['sell_price'] / trades['buy_price']) - 1
+
+        return trades
+    
+    def _calculate_final_balance(self, data, trades, starting_capital=10000):
+        # WIP
+        if trades.empty:
+            return starting_capital
+
+        last_signal = data['signal'].iloc[-1]
+        if last_signal == Action.BUY:
+            current_price = data['Close'].iloc[-1]
+            open_trade_price = data['Close'].iloc[-2]
+            unrealized_pnl = (current_price / open_trade_price) - 1
+        else:
+            unrealized_pnl = 0
+
+        cumulative_return = trades['return'].sum()
+
+        final_balance = starting_capital * (1 + cumulative_return + unrealized_pnl)
+
+        return final_balance
