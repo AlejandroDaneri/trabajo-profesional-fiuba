fef734438a1e5cd1c09820cbe140d291a6962cd7
Sebastian Daniel Ripari <sebastiandripari@gmail.com>
2024-05-17 00:34:51 -0300

    fix(open-trade-ui): sell fields

--- algo_frontend/src/components/Trades.js
+++ algo_frontend/src/components/Trades.js
@@ -41,18 +41,12 @@ const Trades = ({ strategyID }) => {
           amount: parseFloat(trade.amount).toFixed(4),
           sell_timestamp: trade.orders.sell.timestamp,
           buy_timestamp: trade.orders.buy.timestamp,
-          sell_timestamp_label: unixToDate(trade.orders.sell.timestamp),
+          sell_timestamp_label: trade.orders.sell.timestamp ? unixToDate(trade.orders.sell.timestamp) : '',
           buy_timestamp_label: unixToDate(trade.orders.buy.timestamp),
           buy_price: trade.orders.buy.price,
           sell_price: trade.orders.sell.price,
-          duration:
-            (trade.orders.sell.timestamp / 1000 -
-              trade.orders.buy.timestamp / 1000) /
-            60,
-          pl: (
-            (trade.orders.sell.price / trade.orders.buy.price - 1) *
-            100
-          ).toFixed(3),
+          duration:  trade.orders.sell.timestamp ? ((trade.orders.sell.timestamp / 1000 - trade.orders.buy.timestamp / 1000) / 60).toFixed(4) : '',
+          pl: trade.orders.sell.timestamp ? ((trade.orders.sell.price / trade.orders.buy.price - 1) * 100).toFixed(3) : '',
         }
       }
     })
@@ -118,7 +112,7 @@ const Trades = ({ strategyID }) => {
       row.amount,
       row.buy_timestamp_label,
       row.sell_timestamp_label,
-      row.duration.toFixed(4),
+      row.duration,
       row.buy_price,
       row.sell_price,
       <ResultStyle win={row.pl > 0}>{row.pl} </ResultStyle>,
