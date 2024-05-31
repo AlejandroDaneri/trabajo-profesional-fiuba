89179ad042cbe9946a6f07af21703534b2970858
Alejandro Daneri <adaneri@fi.uba.ar>
2024-05-30 23:57:47 -0300

    delete old imports

--- algo_trader/backtester/app.py
+++ algo_trader/backtester/app.py
@@ -1,25 +1,6 @@
-from executors.futures_backtester import FuturesBacktester
-from executors.buy_and_hold import BuyAndHoldBacktester
-from executors.spot_backtester import SpotBacktester
-from lib.utils.utils_backtest import hydrate_strategy
-from lib.utils.plotter import trades_2_balance_series, buy_and_hold_balance_series
-from lib.providers.yahoofinance import YahooFinance
-from lib.providers.binance import Binance
-from lib.indicators import __all__ as indicators_list
-from lib.indicators import *
-from lib.constants.timeframe import DATE_FORMAT, TIMEFRAME_1_DAY
+from flask import Flask, jsonify
 
-from flask import Flask, jsonify, request, abort
-from datetime import datetime,timezone  
-import yfinance as yf
-from services.risk_service import RiskCalculator
-import pandas as pd
-
-import hashlib
-import json
-from flask_caching import Cache
 from flask import Flask
-from flask_caching import Cache
 from routes.backtest_routes import backtest_blueprint
 from routes.indicator_routes import indicator_blueprint
 from config import cache  # Import the cache object from cache.py
